e5ea5db0f09d548ad2386470b0ac4d57ba556a2a
Tiria <fred.boyer@tiria.fr>
2023-01-31 16:55:47 +0100

    wp 6.0+ fix notice

--- html/wp-content/themes/tiria-theme/library/setup.php
+++ html/wp-content/themes/tiria-theme/library/setup.php
@@ -127,7 +127,7 @@
 
 
 	//HTML5
-		add_theme_support( 'html5' );
+		add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption', 'style', 'script' ) );
 
 
 
